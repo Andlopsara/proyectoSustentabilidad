b595240f0112d5e1e2e692f8e5da0da3f458672c
Andrea Lopez <andlopsara25@gmail.com>
2024-11-29 22:10:11 -0600

    ....

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-//componente principal (una parte de la interfaz para que renderice algo por pantalla)
-import logo from './assets/logoU.png';
-import logo1 from './assets/logoSus.png';
 import './App.css';
 import Home from './pages/Home';
-
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <img src={logo1} className="App-logo1" alt="logo" />
-        <Home/>
+      <header className="AppHeader">
+        <Home />
       </header>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import '../styles/Home.css';
+import logo from '../assets/logoU.png';
+import logo1 from '../assets/logoSus.png';
 
 export const Home = () => {
   return (
-    <div className="home-container">
+    <div className="homeContainer">
+      <div>
+        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo1} className="App-logo1" alt="logo" />
+      </div>
       <div>
         <p>BIENVENIDO(A)</p>
       </div>
